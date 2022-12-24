b1af5b648d983eddb276b6883eb57910e1a12228
shtirlizc <shtirlizc@gmail.com>
2022-12-24 06:46:07 +0500

    fix partners

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,21 +1,15 @@
-import Swiper, { Autoplay, EffectFade } from "swiper";
+import Swiper, { Autoplay, EffectFade } from 'swiper';
 
 export const Slider = {
   init() {
-    new Swiper(".js-who-are-we-swiper", {
+    new Swiper('.js-who-are-we-swiper', {
       modules: [Autoplay, EffectFade],
       loop: true,
       speed: 1000,
       autoplay: {
         delay: 4000,
       },
-      effect: "fade",
-    });
-
-    new Swiper(".js-partners-swiper", {
-      loop: true,
-      slidesPerView: "auto",
-      spaceBetween: 60,
+      effect: 'fade',
     });
   },
 };
