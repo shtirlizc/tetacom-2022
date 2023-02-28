e38003e9a5d5fc5afba93f5eab13b1138a61c5c9
shtirlizc <shtirlizc@gmail.com>
2023-02-28 16:06:16 +0500

    remove old files

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,22 @@
 import { textField } from './textField';
 import { phoneMask } from './phoneMask';
 import { mobileMenu } from './mobileMenu';
-import { map } from './map';
 import { Modal } from './modal';
 import { Form } from './modal';
 import { PageYOffset } from './pageYOffset';
-import { startScreenVideo } from './start-screen-video';
-import { PauseVideo } from './pauseVideo';
 import { MainSlider } from './mainSlider';
-import { History } from './history';
 import { Capabilities } from './capabilities';
 import { ProductDemoPlayer } from './productDemoPlayer';
 import { Dropdown } from './dropdown';
 
 window.addEventListener('load', () => {
   PageYOffset.init();
-  startScreenVideo();
   MainSlider.init();
   mobileMenu();
   textField();
   phoneMask();
-  map();
   Modal.init();
   Form.init();
-  PauseVideo.init();
-  History.init();
   Capabilities.init();
   ProductDemoPlayer.init();
   Dropdown.init();
