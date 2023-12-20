f5e74ea50b9cf8455ab4243b61bd2635f3c72a8a
shtirlizc <shtirlizc@gmail.com>
2023-12-20 15:51:09 +0500

    add lightbox for text content

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,7 @@ import { Vacancies } from './vacancies';
 import { StartScreenSwiper } from './start-screen-swiper';
 import { Slider } from './slider';
 import { AccordionBlock } from './accordion';
+import { textContent } from './textContent';
 
 window.Modal = Modal;
 
@@ -34,4 +35,5 @@ window.addEventListener('load', () => {
   Cookie.init();
   Slider.init();
   AccordionBlock.init();
+  textContent();
 });
