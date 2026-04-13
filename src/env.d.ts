cf44bb11cf5dd63b26796e519c24577a6443c906
shtirlizc <shtirlizc@gmail.com>
2026-04-13 16:12:36 +0500

    course

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,19 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="astro/client" />
+
+declare module "accordion-js" {
+  interface AccordionOptions {
+    duration?: number;
+    showMultiple?: boolean;
+  }
+
+  interface AccordionConstructor {
+    new (
+      selectorOrElement: string | HTMLElement | Array<string | HTMLElement>,
+      options?: AccordionOptions,
+    ): unknown;
+  }
+
+  const Accordion: AccordionConstructor;
+  export default Accordion;
+}
