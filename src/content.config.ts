b4abe60f2ad47550466ad457ea87b643979a75f5
shtirlizc <shtirlizc@gmail.com>
2026-04-14 15:17:19 +0500

    vacancies

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+const vacancies = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/vacancies" }),
+  schema: z.object({
+    slug: z.string(),
+    title: z.string(),
+    description: z.string(),
+    department: z.string(),
+    draft: z.boolean(),
+  }),
+});
+
+export const collections = { vacancies };
