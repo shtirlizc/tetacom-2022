722bd2296f03908d9ff753b784a00b5fedb018fc
shtirlizc <shtirlizc@gmail.com>
2026-04-21 15:49:18 +0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import { rehypeImageFigures } from "./src/plugins/rehype-image-figures.js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shtirlizc.github.io",
-  base: "/tetacom-2022",
+  site: "https://tetacom.pro/",
   integrations: [sitemap()],
   markdown: {
     rehypePlugins: [rehypeImageFigures],
