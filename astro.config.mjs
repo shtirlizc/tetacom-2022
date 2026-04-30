e67bee9617c1ef2fc89f2173f2cb5fb25610cc1a
shtirlizc <shtirlizc@gmail.com>
2026-04-30 17:14:13 +0500

    add katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 import { rehypeImageFigures } from "./src/plugins/rehype-image-figures.js";
 
 // https://astro.build/config
@@ -8,7 +10,8 @@ export default defineConfig({
   site: "https://tetacom.pro/",
   integrations: [sitemap()],
   markdown: {
-    rehypePlugins: [rehypeImageFigures],
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeImageFigures, rehypeKatex],
   },
   redirects: {
     "/education": "/education/opz-course",
